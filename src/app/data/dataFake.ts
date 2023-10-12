6084106e1a6b99a9f6dfea4f99a95d36990427bc
ADRIANO SANTOS <adrianofrontend@gmail.com>
2023-10-11 23:16:48 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO EM 3D',
+    description: 'marvel anuncia um novo filme do homem de ferro, confira',
+    photoCover:
+      'https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg',
+  },
+  {
+    id: '2',
+    title: 'Nova Filme do Pantera Negra na Disney +',
+    description: 'esse filme é legal',
+    photoCover: 'https://i.ytimg.com/vi/7akAfWBfymM/hqdefault.jpg',
+  },
+  {
+    id: '3',
+    title: 'Nova série anúnciada na Disney +',
+    description: 'esse filme é legal',
+    photoCover:
+      'https://sm.ign.com/ign_pt/news/o/oscar-isaa/oscar-isaac-says-theres-no-official-word-on-moon-knight-seas_pwm8.jpg',
+  },
+
+  {
+    id: '4',
+    title: 'Novo filme do X-men',
+    description: 'esse filme é legal',
+    photoCover:
+      'https://legadodamarvel.com.br/wp-content/uploads/2023/07/X-Men-Novo-reboot-da-Marvel-ja-tem-previsao-para-chegar-ao-cinema-legadodamarvel.webp',
+  },
+];
